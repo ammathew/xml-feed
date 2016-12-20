64f2480179acf39c5d8872e9e96ab4350aedb148
AnnMary Mathew <annmary1985@gmail.com>
2016-12-20 22:33:00 +0000

    added back MRSS class file

---
+++ lib/XML/Feed/Format/MRSS.pm
@@ -0,0 +1,51 @@
+package XML::Feed::Format::MRSS;
+use strict;
+use warnings;
+
+use XML::Feed:Format::RSS
+
+use XML::FeedPP;
+
+sub init_string {
+
+    my $feed = shift;
+    my($str) = @_;
+
+    #here, convert feedPP to XML::RSS feed
+    $feed->init_empty;
+
+    my $feed_copy = $feed;
+    my $parsed_feed = $feed_copy->{rss}->parse( $$str );
+    my $parsed_feed_items = $parsed_feed->{items};
+    
+    my $blah = XML::FeedPP->new( $$str );
+    
+    $feed->init_empty;
+
+    foreach my $key ( keys $parsed_feed->{channel}   ) { 
+
+        $feed->{rss}->channel( $key => $parsed_feed->{channel}->{ $key } ); 
+    }
+
+
+    foreach my $item ( $blah->get_item() ) { 
+   
+        my $guid = $item->{guid}->{'#text' };
+
+        $item->{guid} = $guid; # this needs to be fixed ... $item->{guid} should already come in as a string
+
+
+        foreach my $parsed_feed_item ( @$parsed_feed_items ) {
+
+            if ( $parsed_feed_item->{'guid'} eq $guid ) {
+
+                $feed->{rss}->add_item( %$item ) 
+          
+            }
+            
+        }
+
+    };
+
+    return $feed
+}
